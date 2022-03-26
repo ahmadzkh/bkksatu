d3603497b9aab071663a3ca84eada4c840a9005e
ahmadzkh <75772659+ahmadzkh@users.noreply.github.com>
2022-03-26 21:03:44 +0700

    Commit Middleware -zaky

---
+++ app/Http/Middleware/IsMitra.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsMitra
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user()->id_level === 'LVL00003'){
+            return $next($request);
+        }
+
+        return redirect('/')->with('error',"You don't have admin access.");
+    }
+}
